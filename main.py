7fc7d5a4e6fd6bc26c52e17f6d90b92b5a514c5a
Grancea Alexandru <alexandrugrancea25@gmail.com>
2023-10-12 22:13:14 +0300

    5 sortings

--- main.py
+++ main.py
@@ -98,11 +98,58 @@ def BuildHeapList(n : int, number_list : list, number_steps : int) -> list:
 
 #selection sort function
 
-def selectionSort(n : int, number_list : int) -> list:
+def selectionSort(n : int, number_list : int, step : int) -> list:
+    number_step = 0
     for i in range(n):
-        for j in range(i, n + 1):
+        for j in range(i, n):
             if number_list[i] > number_list[j]:
                 (number_list[i], number_list[j]) = (number_list[j], number_list[i])
+                number_step = number_step + 1
+                if number_step % step == 0:
+                    print(number_list)
+
+    return number_list
+
+
+#bubble sort function
+
+def bubbleSort(n : int, number_list : int,  step : int) -> list:
+    number_step = 0
+    sorted = 0
+    while(sorted == 0):
+        sorted = 1
+        for i in range(n - 1):
+            if number_list[i] > number_list[i + 1]:
+                (number_list[i], number_list[i + 1]) = (number_list[i + 1], number_list[i])
+                sorted = 0
+
+                number_step = number_step + 1
+                if number_step % step == 0:
+                    print(number_list)
+
+
+    return number_list
+
+
+#bogosort function, shuffle the list till it s sorted
+
+def TestIfSort(n : int, number_list : int) -> bool:
+    for i in range(n - 1):
+        if number_list[i] > number_list[i + 1]:
+            return 0
+    return 1
+
+def randomize_list(n : int, number_list : list) -> list:
+    for i in range(n):
+        index = random.randint(0, n - 1)
+        (number_list[i], number_list[index]) = (number_list[index], number_list[i])
+    return number_list
+
+def bogoSort(n : int, number_list : list) -> list:
+    while TestIfSort(n, number_list) == 0:
+        print(number_list)
+        number_list = randomize_list(n, number_list)
+
     return number_list
 
 #main program
@@ -110,6 +157,9 @@ def selectionSort(n : int, number_list : int) -> list:
 print("Press the option do you want to choose ")
 print("1 for insertion sort")
 print("2 for  heap sort")
+print("3 for selection sort")
+print("4 for bubble sort")
+print("5 for bogo sort")
 which_sort = int(input(">"))
 
 n = int(input("Write the number of numbers you want to generate: "))
@@ -121,7 +171,7 @@ step = int(input("Write the step you want to see the partially sorted list "))
 if which_sort == 1:
     list_of_numbers = insertionSort(n, list_of_numbers, step)
     print(list_of_numbers)
-else:
+elif which_sort == 2:
     #Index the list from 1.
     list_of_numbers.insert(0, 0)
 
@@ -129,3 +179,12 @@ else:
     heap_list = BuildHeapList(n, list_of_numbers, number_steps)
     heap_list = HeapSort(n, heap_list, step, number_steps)
     printHeap(heap_list)
+elif which_sort == 3:
+    list_of_numbers = selectionSort(n, list_of_numbers, step)
+    print(list_of_numbers)
+elif which_sort == 4:
+    list_of_numbers = bubbleSort(n, list_of_numbers, step)
+elif which_sort == 5:
+    print("At bogo sort i will print at every randomize")
+    list_of_numbers = bogoSort(n, list_of_numbers)
+    print(list_of_numbers)
